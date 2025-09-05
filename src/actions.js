ba7f516037b849d6c14c4d4fc3bb0cd038a5a9dd
Bendas <rayushekrin@gmail.com>
2025-09-05 14:00:10 +0200

    Інтеграція Redux у існуючий React проект

---
+++ src/actions.js
@@ -0,0 +1,15 @@
+import { ADD_USER, REMOVE_USER } from "./actionTypes"; 
+
+export function addUser(name) {
+  return {
+    type: ADD_USER,
+    payload: name, 
+  };
+}
+
+export function removeUser(id) {
+  return {
+    type: REMOVE_USER,
+    payload: id, 
+  };
+}
---
+++ src/reducer.js
@@ -0,0 +1,31 @@
+import { ADD_USER, REMOVE_USER } from "./actionTypes";
+
+const initialState = {
+  users: [
+    { id: 1, name: "Жека" },
+    { id: 2, name: "Андрій" },
+    { id: 3, name: "Діма" },
+  ],
+};
+
+function usersReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_USER:
+      return {
+        users: [...state.users, { id: Date.now(), name: action.payload }],
+      };
+
+    case REMOVE_USER:
+      return {
+        users: state.users.filter((u) => u.id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+}
+
+
+
+
+export default usersReducer;
